e909e1a9c7b60fc013e284697db782990b9bb429
Samuel Ding <samuelshijieding@gmail.com>
2024-12-07 09:52:29 -0500

    Day 3 complete

--- 2024/Day 3/Day 3 part 1.py
+++ 2024/Day 3/Day 3 part 1.py
@@ -1,7 +1,15 @@
 from pathlib import Path
+import re
 
 mod_path = Path(__file__).parent
 relative_path_1 = 'Day 3 input.txt'
 src_path_1 = (mod_path / relative_path_1).resolve()
 with open(src_path_1, "r") as a:
-    input_lines = a.readlines()
+    input_stuff = a.read()
+
+possible_muls = re.findall("mul\(([0-9]+),([0-9]+)\)", input_stuff)
+print(possible_muls)
+tot = 0
+for pair in possible_muls:
+    tot += int(pair[0])*int(pair[1])
+print(tot)
--- 2024/Day 3/Day 3 part 2.py
+++ 2024/Day 3/Day 3 part 2.py
@@ -1,7 +1,16 @@
 from pathlib import Path
+import re
 
 mod_path = Path(__file__).parent
 relative_path_1 = 'Day 3 input.txt'
 src_path_1 = (mod_path / relative_path_1).resolve()
 with open(src_path_1, "r") as a:
-    input_lines = a.readlines()
+    input_stuff = a.read()
+
+usable = [i.split("don't()")[0] for i in input_stuff.split("do()")]
+tot = 0
+for string in usable:
+    possible_muls = re.findall(r"mul\(([0-9]+),([0-9]+)\)", string)
+    for pair in possible_muls:
+        tot += int(pair[0])*int(pair[1])
+print(tot)
