1576d1221e054f7471911e5e9a6fa504e33266d9
Samuel Ding <samuelshijieding@gmail.com>
2024-12-10 08:36:05 -0500

    Day 7 complete

---
+++ 2024/Day 7/Day 7 part 2.py
@@ -0,0 +1,32 @@
+from pathlib import Path
+
+mod_path = Path(__file__).parent
+relative_path_1 = 'Day 7 input.txt'
+src_path_1 = (mod_path / relative_path_1).resolve()
+with open(src_path_1, "r") as a:
+    input_lines = a.readlines()
+
+def find_possible(nums, result):
+    if max(nums) > result:
+        return False
+    elif len(nums) == 1:
+        return nums[0] == result
+    else:
+        new_nums = [nums[0]+nums[1]] + nums[2:]
+        if find_possible(new_nums, result):
+            return True
+        new_nums = [nums[0]*nums[1]] + nums[2:]
+        if find_possible(new_nums, result):
+            return True
+        new_nums = [int(str(nums[0])+str(nums[1]))] + nums[2:]
+        if find_possible(new_nums, result):
+            return True
+        return False
+
+tot = 0
+for line in input_lines:
+    result, nums = line.strip().split(": ")
+    nums = [int(i) for i in nums.split(" ")]
+    result = int(result)
+    tot += find_possible(nums, result)*result
+print(tot)
